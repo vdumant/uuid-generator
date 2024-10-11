a2883d34eb27d909fd0cc2579d4571107896942b
vdumant <duman.fabian.g@gmail.com>
2024-10-11 07:53:16 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// Generate a random UUID
+const random_uuid = uuidv4();
+
+// Print the UUID
+console.log(random_uuid);
+
+function uuidv4() {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+    const r = (Math.random() * 16) | 0,
+      v = c == "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
